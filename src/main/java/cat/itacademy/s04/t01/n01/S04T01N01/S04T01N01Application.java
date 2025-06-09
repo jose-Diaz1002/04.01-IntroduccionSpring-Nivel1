c47ac9d5f50163de33a5dd17c6050d796a9369ba
JoseDiaz <jldi1002@hotmail.com>
2025-06-09 23:05:56 +0200

    primer

---
+++ src/main/java/cat/itacademy/s04/t01/n01/S04T01N01/S04T01N01Application.java
@@ -0,0 +1,22 @@
+package cat.itacademy.s04.t01.n01.S04T01N01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01Application.class, args);
+	}
+
+}
+/*
+http://localhost:9000/HelloWorld
+http://localhost:9000/HelloWorld?nombre=???
+
+http://localhost:9000/HelloWorld2
+
+http://localhost:9000/HelloWorld2/???
+ */
+
